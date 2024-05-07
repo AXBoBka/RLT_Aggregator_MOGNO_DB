7357453e59a06d6457ac28ed7e7a690f66b5bb99
AxBoBka <vladimir.harionovskiy@gmail.com>
2024-05-07 17:31:49 +0400

    Ready task

--- aggregator/main.py
+++ aggregator/main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import isodate
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
 
 from database import dbWrapper
@@ -36,12 +37,21 @@ def validate_interval(interval: str):
 
 def prepare_pipeline(db_wrapper: dbWrapper, dt_from: str, dt_upto: str, interval: str):
     iso_dt_from, iso_dt_upto = get_isoformat(dt_from, dt_upto)
+    if interval == 'hour':
+        total_hours = int((iso_dt_upto - iso_dt_from).total_seconds() / 3600)  # Вычисляем общее количество часов
+        date_range = [iso_dt_from + timedelta(hours=i) for i in range(total_hours + 1)]
+    elif interval == 'day':
+        date_range = [iso_dt_from + timedelta(days=i) for i in range((iso_dt_upto - iso_dt_from).days + 1)]
+    else:
+        date_range = [iso_dt_from + relativedelta(months=i) for i in range((iso_dt_upto.year - iso_dt_from.year) * 12 + iso_dt_upto.month - iso_dt_from.month + 1)]
+
+
     pipeline, date_format = db_wrapper.get_aggregation_pipeline(
         iso_dt_from, iso_dt_upto, interval)
-    return pipeline, date_format
+    return pipeline, date_format, date_range
 
 
-def generate_response(result, interval: str, date_format: str):
+def generate_response(result, interval: str, date_format: str, date_range: list[datetime]):
     dataset = []
     labels = []
 
@@ -66,6 +76,15 @@ def generate_response(result, interval: str, date_format: str):
             labels.append(date_iso_format)
         else:
             raise Exception(f'Неизвестный интревал: {interval}')
+        
+    for date in date_range:
+        date_iso = date.isoformat()
+        if date_iso not in labels:
+            for i, date_label in enumerate(labels):
+                if date_label > date_iso:
+                    labels.insert(i, date_iso)
+                    dataset.insert(i, 0)
+                    break
 
     response = {"dataset": dataset, "labels": labels}
     print(response)
@@ -78,9 +97,9 @@ if __name__ == "__main__":
     db_wrapper = dbWrapper(MONGO_URL, DB_NAME)
     collection = db_wrapper.db[COLLECTION_NAME]
 
-    pipeline, date_format = prepare_pipeline(
+    pipeline, date_format, date_range = prepare_pipeline(
         db_wrapper, input_dict['dt_from'], input_dict['dt_upto'], input_dict['group_type'])
 
     result = collection.aggregate(pipeline=pipeline)
 
-    generate_response(result, input_dict['group_type'], date_format)
+    generate_response(result, input_dict['group_type'], date_format, date_range)
